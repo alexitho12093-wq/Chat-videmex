4540945fe9d1f81c45e4909a94b86cfdc00b6a7c
alexitho12093-wq <alexitho12093@gmail.com>
2026-03-01 10:02:39 -0600

    Add Constants for API endpoints and Socket.io configuration

---
+++ android/utils/Constants.kt
@@ -0,0 +1,5 @@
+object Constants {
+    const val API_BASE_URL = "https://api.example.com/"
+    const val SOCKET_IO_URL = "https://socket.example.com/"
+    const val TIMEOUT_DURATION = 30 // seconds
+}
